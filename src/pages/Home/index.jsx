cfd7e03574dbaea7948c463bf0eaf0be7f75c7d0
Lucas12github <Lucasmouhsen@gmail.com>
2023-10-08 19:04:15 -0300

    Arreglo de DrinkRandom

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,10 @@ import DrinkDetailModal from "../../components/DrinkModal";
 import DrinkRandom from "../../components/DrinkRandom";
 import DrinksList from "../../components/DrinksList";
 import SearchForm from "../../components/SearchForm";
-import DrinkRandom from "../../components/DrinkRandom";
 
 export default function Home() {
     return (
         <main>
-            <DrinkRandom/>
             <DrinkRandom/>
             <SearchForm />
             <DrinksList />
